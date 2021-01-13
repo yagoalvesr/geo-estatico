37d832308293b4703e69cd9e37c26e6613a5a9b6
Yago Alves Ribeiro <yago.alves.r@hotmail.com>
2021-01-13 19:33:34 -0300

    Ajuste baseUrl

--- src/app/shared/service/abstract.service.ts
+++ src/app/shared/service/abstract.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 @Injectable({
@@ -9,7 +9,7 @@ export class AbstractService {
   protected baseUrl: string;
 
   constructor() {
-    // this.baseUrl = 'https://geo-spring.herokuapp.com';
-    this.baseUrl = 'http://localhost:8080';
+    this.baseUrl = 'https://geo-spring.herokuapp.com';
+    // this.baseUrl = 'http://localhost:8080';
   }
 }
